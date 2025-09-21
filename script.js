f5eb7367a2a8840d21e2a5d37f95a094ef5703ea
alifahmed007 <alifahammead001@gmail.com>
2025-09-21 06:36:30 +0600

    add coin area

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ for (let i = 0; i < heartElements.length; i++) {
     });
 }
 
+// copy Area    
+
+const copyButton = document.getElementsByClassName("copy-button");
+const copyCountElement = document.getElementById("copy-count");
+
+let copyCount = 0;
+
+for (let i = 0; i < copyButton.length; i++) {
+    copyButton[i].addEventListener("click", function () {
+        const cart = this.closest(".shadow-md"); 
+        const cartName = cart.querySelector(".copy-text").innerText;
+
+        navigator.clipboard.writeText(cartName)
+            .then(() => {
+
+  
+
+                copyCount++; 
+                copyCountElement.innerText = copyCount;
+                alert("Product name copied: " + cartName);
+            })
+            .catch(err => { 
+                console.error('Failed to copy text: ', err);
+            });
+    });
+}
+
